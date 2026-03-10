bb3d290ce75f500fd614cb76a01607a79e057eaf
RedEyeCentipede <yashrajjjsahu@gmail.com>
2026-03-10 23:19:57 +0530

    Add files via upload
    Variable in js

---
+++ Variable.js
@@ -0,0 +1,27 @@
+/*
+Variable 
+In JavaScript, variables are used to store data values (numbers, text, objects, etc.).
+*/
+
+    // const is used when the value should not change.
+    const accountId = 4467
+
+    // let is the recommended way when the value may change.
+    let accountEmail = "yashsahu1oth@gmail.com" 
+
+    // var was the original way to declare variables in JavaScript.
+     // Var is not preferable to use let is more prefered 
+    var accountPass = "1234"
+
+    accountCity = "Bhopal"
+
+    // accountId =  not allowed as const can't be defined twise 
+
+    accountEmail = "yashduu@gmail.com"
+    accountPass = "22222"
+    accountCity = "Gadarwara"
+
+    console.log(accountId);
+    console.table([accountId,accountEmail,accountPass,accountCity])
+    
+    
