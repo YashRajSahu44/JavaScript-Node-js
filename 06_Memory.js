5d36bf7b9c137686c2ea85740c8a8bc87a984eaf
Yashraj Sahu <yashrajjjsahu@gmail.com>
2026-03-13 23:50:35 +0530

    Update 06_Memory.js

--- 06_Memory.js
+++ 06_Memory.js
@@ -1,7 +1,9 @@
  //Stack memory - Primitive
  //stack memory is used to store primitive variables and function calls.
 
+
  //Heap memory - non primitive
  //Heap memory is the part of RAM where objects and complex data types are stored in JavaScript.
+
 
 //Unlike stack memory, heap memory is large, slower, and used for dynamic memory allocation.
